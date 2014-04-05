d4a4666188eb798503a02bafb3152e2be5721d48
Toni Schmidbauer <toni@stderr.at>
2014-04-05 14:03:21 +0200

    import test modules

---
+++ modules/linuxwochen2014/manifests/init.pp
@@ -0,0 +1,16 @@
+class linuxwochen2014 (
+  $ensure = present
+  ) {
+  user { 'toni':
+    ensure => $ensure,
+    uid    => 4711,
+    gid    => 4711,
+  }
+
+  package { 'emacs':
+    ensure => installed
+  } ->
+  package { 'vi':
+    ensure => absent,
+  }
+}
